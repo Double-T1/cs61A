d9ffaeed61f470bbfdf98f65defcb3d090769e5d
chen weichen <newsforcwc@gmail.com>
2023-01-28 15:49:44 +0800

    phase1 done

--- projects/scheme/scheme_reader.py
+++ projects/scheme/scheme_reader.py
@@ -134,17 +134,17 @@ def scheme_read(src):
     val = src.pop_first() # Get the first token
     if val == 'nil':
         # BEGIN PROBLEM 1
-        "*** YOUR CODE HERE ***"
+        return nil
         # END PROBLEM 1
     elif val == '(':
         # BEGIN PROBLEM 1
-        "*** YOUR CODE HERE ***"
+        return read_tail(src)
         # END PROBLEM 1
     elif val in quotes:
         # BEGIN PROBLEM 6
         "*** YOUR CODE HERE ***"
         # END PROBLEM 6
-    elif val not in DELIMITERS:
+    elif val not in DELIMITERS: #DELIMITERS are symbols for error
         return val
     else:
         raise SyntaxError('unexpected token: {0}'.format(val))
@@ -161,11 +161,12 @@ def read_tail(src):
             raise SyntaxError('unexpected end of file')
         elif src.current() == ')':
             # BEGIN PROBLEM 1
-            "*** YOUR CODE HERE ***"
+            src.pop_first()
+            return nil
             # END PROBLEM 1
         else:
             # BEGIN PROBLEM 1
-            "*** YOUR CODE HERE ***"
+            return Pair(scheme_read(src), read_tail(src))
             # END PROBLEM 1
     except EOFError:
         raise SyntaxError('unexpected end of file')
